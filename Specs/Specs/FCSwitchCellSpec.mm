363a054a30f2994d823b7c42b843107dbcc1ef17
Fabian Canas <fcanas@gmail.com>
2012-10-21 22:11:43 -0600

    Adding Cedar again.

---
+++ Specs/Specs/FCSwitchCellSpec.mm
@@ -0,0 +1,16 @@
+#import "FCSwitchCell.h"
+
+using namespace Cedar::Matchers;
+using namespace Cedar::Doubles;
+
+SPEC_BEGIN(FCSwitchCellSpec)
+
+describe(@"FCSwitchCell", ^{
+    __block FCSwitchCell *model;
+
+    beforeEach(^{
+
+    });
+});
+
+SPEC_END
---
+++ Specs/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Specs
+//
+//  Created by Fabian Canas on 10/21/12.
+//  Copyright (c) 2012 Fabian Canas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <Cedar-iOS/Cedar-iOS.h>
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, @"CedarApplicationDelegate");
+  }
+}
